d90565a7ac9c6ce7d8f5800e9c536c6d13e0ecb2
eboxnet <manos@eboxnet.com>
2020-11-22 20:38:53 +0200

    Updates

---
+++ precache-manifest.85056db9c0ceb637bc649038419ad137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302dadb36674da49d12cd71258d30ec8",
+    "url": "/react-budget-app/index.html"
+  },
+  {
+    "revision": "62023ba31479799548ae",
+    "url": "/react-budget-app/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/react-budget-app/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-budget-app/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62023ba31479799548ae",
+    "url": "/react-budget-app/static/js/main.01c8f184.chunk.js"
+  },
+  {
+    "revision": "16970b8403d1b8b1e6aa",
+    "url": "/react-budget-app/static/js/runtime-main.cdb52ff9.js"
+  }
+]);
